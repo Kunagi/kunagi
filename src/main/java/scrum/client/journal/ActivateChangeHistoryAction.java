201d655eefc0d79ece2158d4a9b42567dd1d1d62
Witoslaw Koczewski <wi@koczewski.de>
2015-11-26 15:38:41 +0100

    iss1128 "Show change history" action collapses blocks

--- src/main/java/scrum/client/journal/ActivateChangeHistoryAction.java
+++ src/main/java/scrum/client/journal/ActivateChangeHistoryAction.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
  */
@@ -19,7 +19,7 @@ import ilarkesto.core.scope.Scope;
 import scrum.client.common.AScrumAction;
 import scrum.client.common.AScrumGwtEntity;
 import scrum.client.common.TooltipBuilder;
-import scrum.client.workspace.ProjectWorkspaceWidgets;
+import scrum.client.core.EventBus;
 
 public class ActivateChangeHistoryAction extends AScrumAction {
 
@@ -43,8 +43,7 @@ public class ActivateChangeHistoryAction extends AScrumAction {
 	@Override
 	protected void onExecute() {
 		Scope.get().getComponent(ChangeHistoryManager.class).activateChangeHistory(entity);
-		Scope.get().getComponent(ProjectWorkspaceWidgets.class).showEntity(entity);
-		entity.updateLastModified();
+		EventBus.get().visibleDataChanged();
 	}
 
 }
