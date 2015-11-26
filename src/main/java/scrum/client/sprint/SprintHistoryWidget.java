bbf0d339d75b917d5cbee6973862e5249d26ae0a
Witoslaw Koczewski <wi@koczewski.de>
2015-11-26 14:06:13 +0100

    iss1242 Bugfix: Jumping to sprint history

--- src/main/java/scrum/client/sprint/SprintHistoryWidget.java
+++ src/main/java/scrum/client/sprint/SprintHistoryWidget.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
  */
@@ -23,6 +23,7 @@ import java.util.List;
 import java.util.Map;
 
 import scrum.client.common.AScrumWidget;
+import scrum.client.core.EventBus;
 import scrum.client.project.Requirement;
 import scrum.client.workspace.PagePanel;
 
@@ -80,17 +81,65 @@ public class SprintHistoryWidget extends AScrumWidget {
 		return null;
 	}
 
-	public boolean select(Task task) {
-		Sprint sprint = getSprint(task.getRequirement());
-		if (sprint == null) return false;
+	public boolean select(final Task task) {
+		final Sprint sprint = getSprint(task.getRequirement());
+		if (sprint == null) {
+			new RequestHistoryServiceCall().execute(new Runnable() {
+
+				@Override
+				public void run() {
+					EventBus.get().visibleDataChanged();
+					select(task);
+				}
+			});
+			return false;
+		}
+
+		if (!sprint.historyLoaded) {
+			new RequestHistorySprintServiceCall(sprint.getId()).execute(new Runnable() {
+
+				@Override
+				public void run() {
+					sprint.historyLoaded = true;
+					EventBus.get().visibleDataChanged();
+					SprintHistorySprintWidget sprintHistorySprintWidget = sprintWidgets.get(sprint);
+					sprintHistorySprintWidget.selectTask(task);
+				}
+			});
+			return false;
+		}
 
 		SprintHistorySprintWidget sprintHistorySprintWidget = sprintWidgets.get(sprint);
 		return sprintHistorySprintWidget.selectTask(task);
 	}
 
-	public boolean select(Requirement requirement) {
-		Sprint sprint = getSprint(requirement);
-		if (sprint == null) return false;
+	public boolean select(final Requirement requirement) {
+		final Sprint sprint = getSprint(requirement);
+		if (sprint == null) {
+			new RequestHistoryServiceCall().execute(new Runnable() {
+
+				@Override
+				public void run() {
+					EventBus.get().visibleDataChanged();
+					select(requirement);
+				}
+			});
+			return false;
+		}
+
+		if (!sprint.historyLoaded) {
+			new RequestHistorySprintServiceCall(sprint.getId()).execute(new Runnable() {
+
+				@Override
+				public void run() {
+					sprint.historyLoaded = true;
+					EventBus.get().visibleDataChanged();
+					SprintHistorySprintWidget sprintHistorySprintWidget = sprintWidgets.get(sprint);
+					sprintHistorySprintWidget.selectRequirement(requirement);
+				}
+			});
+			return false;
+		}
 
 		SprintHistorySprintWidget sprintHistorySprintWidget = sprintWidgets.get(sprint);
 		return sprintHistorySprintWidget.selectRequirement(requirement);
--- src/main/java/scrum/server/project/HomepageUpdater.java
+++ src/main/java/scrum/server/project/HomepageUpdater.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
  */
@@ -330,7 +330,7 @@ public class HomepageUpdater {
 		}
 		context.put("date", Tm.FORMAT_WEEKDAY_LONGMONTH_DAY_YEAR_HOUR_MINUTE.format(comment.getDateAndTime()));
 		context.put("dateDe", new SimpleDateFormat("EEE, d. MMMMM yyyy, HH:mm", Locale.GERMANY).format(comment
-			.getDateAndTime().toJavaDate()));
+				.getDateAndTime().toJavaDate()));
 	}
 
 	private void fillRelease(ContextBuilder context, Release release) {
@@ -369,7 +369,6 @@ public class HomepageUpdater {
 		context.put("id", sprint.getId());
 		context.put("reference", sprint.getReference());
 		context.put("label", toHtml(sprint.getLabel()));
-		context.put("goal", wikiToHtml(sprint.getGoal()));
 		context.put("goal", wikiToHtml(sprint.getGoal()));
 		if (sprint.isBeginSet()) {
 			context.put("begin", Tm.FORMAT_LONGMONTH_DAY_YEAR.format(sprint.getBegin()));
