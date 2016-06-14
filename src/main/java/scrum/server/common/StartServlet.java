b7c6cc4cf1cff41d38ee2134f5b2a9fa9f137190
Witoslaw Koczewski <wi@koczewski.de>
2016-06-14 09:06:19 +0200

    integrating bootstrap

--- src/main/java/scrum/server/common/StartServlet.java
+++ src/main/java/scrum/server/common/StartServlet.java
@@ -59,9 +59,12 @@ public class StartServlet extends AKunagiServlet {
 		if (systemConfig.isInstanceNameSet()) title += " @ " + systemConfig.getInstanceName();
 		html.startHEAD(title);
 		html.LINKfavicon();
+
 		html.startSTYLEcss();
 		html.html(getCss());
 		html.endSTYLE();
+
+		html.LINKcss("node_modules/bootstrap/dist/css/bootstrap.min.css");
 		html.SCRIPTjavascript("scrum.ScrumGwtApplication/scrum.ScrumGwtApplication.nocache.js", null);
 		html.SCRIPTjavascript("codemirror/js/codemirror.js", null);
 		html.endHEAD();
@@ -70,6 +73,10 @@ public class StartServlet extends AKunagiServlet {
 		html.comment(applicationInfo.toString());
 		String analyticsId = systemConfig.getGoogleAnalyticsId();
 		if (analyticsId != null) html.googleAnalytics(analyticsId);
+
+		html.SCRIPTjavascript("node_modules/jquery/dist/jquery.min.js", null);
+		html.SCRIPTjavascript("node_modules/bootstrap/dist/js/bootstrap.min.js", null);
+
 		html.endBODY();
 
 		html.endHTML();
